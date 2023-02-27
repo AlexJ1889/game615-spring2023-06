518d635137dc5d480e2f6dc370e5cf5c70c20b7d
AlexJ1889 <alexj1889@gmail.com>
2023-02-27 12:29:46 -0500

    added GameManager

--- exercise06/Assets/Scripts/GameManager.cs
+++ exercise06/Assets/Scripts/GameManager.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro; 
 
 public class GameManager : MonoBehaviour
 {
+
+    int playerScore = 0;
+    int enemyScore = 0; 
+    public TMP_Text playerText;
+    public TMP_Text enemyText;
+    public TMP_Text timerText;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +22,17 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void IncrementPlayerScore()
+    {
+        playerScore += 1;
+        playerText.text = playerScore.ToString();
+    }
+
+    public void IncrementEnemyScore()
+    {
+        enemyScore += 1;
+        enemyText.text = enemyScore.ToString();
     }
 }
