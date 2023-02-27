8ebe85f450e27f15240bc3a8587fe431539eaa57
AlexJ1889 <alexj1889@gmail.com>
2023-02-27 12:52:39 -0500

    added AI elements

---
+++ exercise06/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyScript : MonoBehaviour
+{
+    NavMeshAgent nma;
+    float newPositionTimer = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nma = gameObject.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        newPositionTimer -= Time.deltaTime;
+        if (newPositionTimer < 0)
+        {
+            newPositionTimer = Random.Range(1, 15);
+            Vector3 randomPosition = RandomNavMeshLocation(Random.Range(5, 10));
+            nma.SetDestination(randomPosition);
+        }
+    }
+
+    public Vector3 RandomNavMeshLocation(float radius)
+    {
+        Vector3 randomDirection = Random.insideUnitSphere * radius;
+        randomDirection += transform.position;
+        NavMeshHit hit;
+        Vector3 finalPosition = Vector3.zero;
+        if (NavMesh.SamplePosition(randomDirection, out hit, radius, 1))
+        {
+            finalPosition = hit.position; 
+        }
+
+        return finalPosition;
+    }
+}
