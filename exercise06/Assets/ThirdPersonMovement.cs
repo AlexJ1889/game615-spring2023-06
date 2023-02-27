fb93072066ea4bd0ffac739455f90c363d47fc24
MBMesfin <120344206+MBMesfin@users.noreply.github.com>
2023-02-26 21:38:47 -0500

    push

--- exercise06/Assets/ThirdPersonMovement.cs
+++ exercise06/Assets/ThirdPersonMovement.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class ThirdPersonMovement : MonoBehaviour
 {
+
+    public CharacterController controller;
+
+    public float speed = 6f;
     // Start is called before the first frame update
     void Start()
     {
